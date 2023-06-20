9bb9a5ad8d7efa2ba8be6118c991fbe2003d9f91
TomNdagala <officialtomndagala@gmail.com>
2023-06-20 10:53:28 -0700

    the code is never wrong

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 for + -1 for - and 0 for all other 
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+       	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else 
+	{
+	_putchar(48);
+	}
+	return (0);
+}
