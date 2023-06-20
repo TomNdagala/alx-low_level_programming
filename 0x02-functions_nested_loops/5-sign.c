08a1987c335c5f8f3f5ac1d063703e9ec9326cea
TomNdagala <officialtomndagala@gmail.com>
2023-06-20 11:30:31 -0700

    please nah

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 for positive num, -1 for negative num or zero for anything else 
+ * @n: the int to check 
  */
 int print_sign(int n)
 {
