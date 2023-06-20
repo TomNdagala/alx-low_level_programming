0e77374a5bfe2b3e0d5436d66e89fe6d0c673800
TomNdagala <officialtomndagala@gmail.com>
2023-06-20 11:27:03 -0700

    Lesson learnt

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 for a positive num, -1 for a negative num or zero for anything else 
+ * Return: 1 for positive num, -1 for negative num or zero for anything else 
  */
 int print_sign(int n)
 {
@@ -20,6 +20,6 @@ int print_sign(int n)
 	else
 	{
 	_putchar (48);
-	return(0);
+	return (0);
 	}
 }
